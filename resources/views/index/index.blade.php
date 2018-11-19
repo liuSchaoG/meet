9d3730fd18986c2513a6a5e7c3aa08a224484ddc
liuchao <charles_c_liu@163.com>
2018-11-19 17:11:37 +0800

    update

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-heading">欢迎页</div>
 
                     <div class="panel-body">
-
+                        欢迎访问！
                     </div>
                 </div>
             </div>
