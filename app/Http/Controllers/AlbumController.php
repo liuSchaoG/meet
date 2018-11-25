fd7e695ef7d444755a0288e39b2b4c035b88efaa
liuchao <charles.c.liu@xiaolaba.org>
2018-11-25 21:34:27 +0800

    date

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -6,6 +6,7 @@ use App\Services\AlbumService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Extensions\ReturnModel;
+use App\Models\Picture;
 
 /**
  * 相册处理模块
@@ -64,9 +65,11 @@ class AlbumController extends Controller
      */
     public function uploadAlbum(Request $request)
     {
-        // $uid = session('id');
+        
         //显示的属性更多
         $fileCharater = $request->file('file');
+
+        $alb_id = $request->alb_id;
 
         if ($fileCharater->isValid()) { //括号里面的是必须加的哦
             //如果括号里面的不加上的话，下面的方法也无法调用的
@@ -76,17 +79,22 @@ class AlbumController extends Controller
 
             //获取文件的绝对路径
             $path = $fileCharater->getRealPath();
-
+            $storage_path = md5($alb_id);
+            $name = md5(date('Y-m-dhis')).'.'.$ext;
             //定义文件名
-            $filename = date('Y-m-d-h-i-s').'.'.$ext;
+            $filename = $storage_path.'/'.$name;
             //存储文件。disk里面的public。总的来说，就是调用disk模块里的public配置
             Storage::disk('upload')->put($filename, file_get_contents($path));
-
-            // $insert['']
-
+            
+            $insert['uid'] = session('id');
+            $insert['type'] = 1;
+            $insert['alb_id'] = $alb_id;
+            $insert['img'] = '/upload/'.$filename;
+            $insert['name'] = $name;
+            Picture::insert($insert);
         }
 
-    
+        return redirect()->route('pictureList',['alb_id'=>$alb_id]);
     }
 
 
