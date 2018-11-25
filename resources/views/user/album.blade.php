0cb17a0976351a441ca9f8c0f0e99cb3ca5d9efd
liuchao <charles.c.liu@xiaolaba.org>
2018-11-25 19:15:53 +0800

    date

--- resources/views/user/album.blade.php
+++ resources/views/user/album.blade.php
@@ -9,7 +9,7 @@
                     <div class="row clearfix">
                         @forelse ($list as $value)
                             <div class="col-md-3 column">
-                                <a href=""><img class="col-md-3 column" src="{{$value['img']}}" alt=""></a>
+                                <a href=""><img style="width: 50px" src="{{$value['img']}}" alt=""></a>
                                 <p>
                                      <a class="btn" href="">{{$value['name']}} »</a>
                                 </p>
