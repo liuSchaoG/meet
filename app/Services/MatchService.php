e33032ab10702ab48db9e4ab5b52b781fa7249a9
liuchao <charles.c.liu@xiaolaba.org>
2018-12-06 08:53:00 +0800

    匹配检索模块

---
+++ app/Services/MatchService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\User;
+use App\Models\Preference;
+use App\Models\UserInfo;
+use Illuminate\Support\Facades\DB;
+/**
+ * 匹配信息服务提供
+ */
+class MatchService 
+{
+
+    /**
+     * 获取默认匹配的列表
+     * @author liuchao 2018-12-05T22:30:56+0800
+     * @param  [type] $pid [description]
+     * @return [type]      [description]
+     */
+    public function getDefaultList()
+    {
+        
+    }
+
+
+    
+
+}
