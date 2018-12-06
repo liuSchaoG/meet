e39968d890f27bd3295c313bc7301e6eb197621c
liuchao <charles_c_liu@163.com>
2018-12-06 19:06:50 +0800

    update

--- app/Http/Controllers/MatchingController.php
+++ app/Http/Controllers/MatchingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\MatchService;
+use App\Extensions\GlobalFunction;
 /**
  * 列表用户控制器
  */
@@ -30,8 +31,13 @@ class MatchingController extends Controller
     public function index(Request $request)
     {
         $param = $request->all(); 
-        //获取浏览器地理位置
-        
+        //获取浏览器地理位置  
+        $locations = GlobalFunction::getCityByIp();
+        if($locations){
+            $param['local_country'] = $locations['data']['country'];
+            $param['local_province'] = $locations['data']['region'];
+            $param['local_city'] = $locations['data']['city'];
+        }
         $list = $this->matchService -> getDefaultList($param);
         
         return view('home');
