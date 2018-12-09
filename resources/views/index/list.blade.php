4c247cf25d75ffe2ac83070893aae76928a56d35
liuchao <charles.c.liu@xiaolaba.org>
2018-12-09 11:16:34 +0800

    update

--- resources/views/index/list.blade.php
+++ resources/views/index/list.blade.php
@@ -14,20 +14,20 @@
                                   <div class="info-primary">
                                       <h3 class="name">
                                           <a href="{{$info->uid}}" data-itemid="1" data-lid="1cuR2yKgA43.search" data-jobid="29925123" data-index="1" ka="search_list_1" target="_blank">
-                                              <span>{{$info->height}}</span>&nbsp;<span class="red">{{$info->income}}</span>
+                                              <span>{{$info->username}}</span>&nbsp;<span class="red">666</span>
                                           </a>
                                       </h3>
-                                      <p>北京<em class="vline"></em>3-5年<em class="vline"></em>本科</p>
+                                      <p>{{$info->area_name}}<em class="vline"></em>{{$info->income}}<em class="vline"></em>{$info->education}}</p>
                                   </div>
                                   <div class="info-company">
-                                      <div class="company-text">遇见网络</p>
+                                      <div class="company-text">{{$info->college}}</p>
                                       </div>
                                   </div>
                                   <div class="info-publis">
-                                      <h3 class="name"><img src="https://img2.bosszhipin.com/boss/avatar/avatar_14.png?x-oss-process=image/resize,w_40,limit_0">{{$info->user_name}}<em class="vline"></em>求婚者</h3>
-                                      <p>发布于11月19日</p>
+                                      <h3 class="name"><img src="{{$info->head_image}}">{{$info->user_name}}<em class="vline"></em>{{$info->username}}</h3>
+                                      <p>创建于{{$info->created_at}}</p>
                                   </div>
-                                  <a href="javascript:;" data-url="{{$info->uid}}" redirect-url="/geek/new/index/chat?id=6c0de8711ede6e851Hx82t29ElU~" target="_blank" class="btn btn-startchat">了解一下
+                                  <a href="javascript:;" data-url="/chatRoom?uid={{$info->uid}}" target="_blank" class="btn btn-startchat">立刻开撩
                                   </a>
                               </div>
                         </li>
