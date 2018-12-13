46a1d4d4cf79109d67c2c98a127943ce571d4182
liuchao <charles_c_liu@163.com>
2018-12-13 16:15:48 +0800

    update

--- app/Services/MatchService.php
+++ app/Services/MatchService.php
@@ -37,7 +37,7 @@ class MatchService
         foreach ($list_p as $key => $value) {
         	$info = User::findByUid($value['uid']);
         	$value['job_name'] = Position::findNameById($value['job']);
-        	$value['head_img'] = $info['head_img'];
+        	$value['head_img'] = $info['head_image'];
         	$value['created_at'] = $info['created_at'];
         	$value['username'] = $info['username'];
         	$value['area_name'] = Area::findNameById($value['area_city']);
